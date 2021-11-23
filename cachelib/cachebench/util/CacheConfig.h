4f2a56255bdaf940de95a62c1a5312a7d4710e26
victoria-mcgrath <victoria.mcgrath@intel.com>
2021-11-23 09:53:58 -0800

    Enabled shared memory tier in cachebench.

--- cachelib/cachebench/util/CacheConfig.h
+++ cachelib/cachebench/util/CacheConfig.h
@@ -46,15 +46,22 @@ struct MemoryTierConfig : public JSONConfig {
 
   explicit MemoryTierConfig(const folly::dynamic& configJson);
   MemoryTierCacheConfig getMemoryTierCacheConfig() {
-    if (file.empty()) {
-      throw std::invalid_argument("Please specify valid path to memory mapped file.");
-    }
-    MemoryTierCacheConfig config = MemoryTierCacheConfig::fromFile(file).setRatio(ratio);
+    MemoryTierCacheConfig config = memoryTierCacheConfigFromSource();
+    config.setRatio(ratio);
     return config;
   }
 
   std::string file{""};
   size_t ratio{0};
+
+private:
+  MemoryTierCacheConfig memoryTierCacheConfigFromSource() {
+    if (file.empty()) {
+      return MemoryTierCacheConfig::fromShm();
+    } else {
+      return MemoryTierCacheConfig::fromFile(file);
+    }
+  }
 };
 
 struct CacheConfig : public JSONConfig {
