c8576f5747d74b3b4d9f8f051fe3f6972b047fb2
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2021-12-10 21:45:58 -0500

    Extend CompressedPtr to work with multiple tiers
    Now it's size is 8 bytes intead of 4.
    
    Original CompressedPtr stored only some offset with a memory Allocator.
    For multi-tier implementation, this is not enough. We must also store
    tierId and when uncompressing, select a proper allocator.
    
    An alternative could be to just resign from CompressedPtr but they
    are leveraged to allow the cache to be mapped to different addresses on shared memory.
    
    Changing CompressedPtr impacted CacheItem size - it increased from 32 to 44 bytes.

--- cachelib/allocator/memory/SlabAllocator.cpp
+++ cachelib/allocator/memory/SlabAllocator.cpp
@@ -527,6 +527,8 @@ serialization::SlabAllocatorObject SlabAllocator::saveState() {
 // for benchmarking purposes.
 const unsigned int kMarkerBits = 6;
 CompressedPtr SlabAllocator::compressAlt(const void* ptr) const {
+  // XXX: do we need to set tierId here?
+
   if (ptr == nullptr) {
     return CompressedPtr{};
   }
@@ -538,6 +540,8 @@ CompressedPtr SlabAllocator::compressAlt(const void* ptr) const {
 }
 
 void* SlabAllocator::unCompressAlt(const CompressedPtr cPtr) const {
+  // XXX: do we need to set tierId here?
+
   if (cPtr.isNull()) {
     return nullptr;
   }
