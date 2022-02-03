9b1e564362a683e9d8e081a4546f6c2f79f7fed6
Victoria McGrath <victoria.mcgrath@intel.com>
2022-02-02 20:41:54 -0500

    Single-threaded insertion tests

--- cachelib/allocator/tests/MemoryTiersTest.cpp
+++ cachelib/allocator/tests/MemoryTiersTest.cpp
@@ -28,6 +28,8 @@ using LruMemoryTierConfigs = LruAllocatorConfig::MemoryTierConfigs;
 using Strings = std::vector<std::string>;
 using SizePair = std::tuple<size_t, size_t>;
 using SizePairs = std::vector<SizePair>;
+using StringDataKeyValue = std::pair<std::string, std::string>;
+using StringDataKeyValues = std::vector<StringDataKeyValue>;
 
 const size_t defaultTotalCacheSize{1 * 1024 * 1024 * 1024};
 const std::string defaultCacheDir{"/var/metadataDir"};
@@ -87,9 +89,15 @@ class MemoryTiersTest: public AllocatorTest<Allocator> {
       LruMemoryTierConfigs tierConfigs;
       tierConfigs.reserve(tierPaths.size());
       for(auto i = 0; i < tierPaths.size(); ++i) {
-        tierConfigs.push_back(MemoryTierCacheConfig::fromFile(tierPaths[i])
-                              .setRatio(std::get<0>(sizePairs[i]))
-                              .setSize(std::get<1>(sizePairs[i])));
+        if (tierPaths[i].empty()) {
+          tierConfigs.push_back(MemoryTierCacheConfig::fromShm()
+                                .setRatio(std::get<0>(sizePairs[i]))
+                                .setSize(std::get<1>(sizePairs[i])));
+        } else {
+          tierConfigs.push_back(MemoryTierCacheConfig::fromFile(tierPaths[i])
+                                .setRatio(std::get<0>(sizePairs[i]))
+                                .setSize(std::get<1>(sizePairs[i])));
+        }
       }
       cfg.configureMemoryTiers(tierConfigs);
       return cfg;
@@ -182,6 +190,113 @@ TEST_F(LruMemoryTiersTest, TestInvalid2TierConfigSizesNeCacheSize) {
                std::invalid_argument);
 }
 
+void generateAndInsert(LruAllocatorConfig& cfg,
+                       const std::pair<size_t, size_t>& keyRange,
+                       const size_t itemSize) {
+  using Item = typename LruAllocator::Item;
+  using RemoveCbData = typename LruAllocator::RemoveCbData;
+  size_t itemsInserted = 0, itemsFound = 0, itemsEvicted = 0;
+  std::set<std::string> movedKeys;
+  std::vector<RemoveCbData> removedData;
+  std::unique_ptr<LruAllocator> alloc;
+
+  auto moveCb = [&](const Item& oldItem, Item& newItem, Item*) {
+    std::memcpy(newItem.getWritableMemory(), oldItem.getMemory(),
+                oldItem.getSize());
+    movedKeys.insert(oldItem.getKey().str());
+  };
+
+  auto removeCb = [&](const RemoveCbData& data) {
+    removedData.push_back(data);
+  };
+
+  cfg.setRemoveCallback(removeCb);
+  cfg.enableMovingOnSlabRelease(moveCb);
+
+  if (cfg.getMemoryTierConfigs().size() > 1) {
+    alloc = std::unique_ptr<LruAllocator>(new LruAllocator(LruAllocator::SharedMemNew, cfg));
+  } else {
+    alloc = std::unique_ptr<LruAllocator>(new LruAllocator(cfg));
+  }
+
+  const size_t numBytes = alloc->getCacheMemoryStats().cacheSize;
+  auto poolId = alloc->addPool("default", numBytes);
+
+  for(auto i = keyRange.first; i < keyRange.second; ++i) {
+    auto key = std::to_string(i);
+    std::string value = "";
+    for (size_t j = 0; j < itemSize / key.length(); ++j) {
+      value += key;
+    }
+    value += key.substr(0, itemSize % key.length());
+    auto h = alloc->allocate(poolId, key, value.length());
+    EXPECT_TRUE(h);
+
+    std::memcpy(h->getWritableMemory(), value.c_str(), value.length());
+
+    if (alloc->insert(h)) {
+      ++itemsInserted;
+    }
+  }
+
+  for(auto i = keyRange.first; i < keyRange.second; ++i) {
+    auto key = std::to_string(i);
+    auto x = alloc->find(key);
+    if (x) {
+      ++itemsFound;
+    }
+  }
+
+  for (auto it = removedData.begin(); it != removedData.end(); ++it) {
+    auto x = alloc->find((*it).item.getKey().str());
+    if (x && ((*it).context == facebook::cachelib::RemoveContext::kEviction)) {
+      ++itemsEvicted;
+    }
+  }
+
+  std::cout << "alloc.getCacheMemoryStats().cacheSize: " << numBytes << " (" << "cfg.getCacheSize(): " << cfg.getCacheSize() << ")";
+  std::cout << "; Tiers: " << ((cfg.getMemoryTierConfigs().size() > 1)? "true" : "false");
+  std::cout << "; Items inserted: " << itemsInserted;
+  std::cout << "; Items found: " << itemsFound;
+  std::cout << "; Items evicted: " << itemsEvicted << " (total items removed: " << removedData.size() << ")"<< std::endl;
+}
+
+LruAllocatorConfig create2TierCacheConfig(size_t totalCacheSize) {
+  LruAllocatorConfig tieredCacheConfig{};
+  tieredCacheConfig.setCacheSize(totalCacheSize)
+    .enableCachePersistence(folly::sformat("/tmp/multi-tier-test/{}", ::getpid()))
+    .usePosixForShm()
+    .configureMemoryTiers(
+      {MemoryTierCacheConfig::fromFile(folly::sformat("/tmp/tier1{}", ::getpid())).setRatio(1),
+       MemoryTierCacheConfig::fromFile(folly::sformat("/tmp/tier2{}", ::getpid())).setRatio(1)});
+  return tieredCacheConfig;
+}
+
+LruAllocatorConfig createDramCacheConfig(size_t totalCacheSize) {
+  LruAllocatorConfig dramConfig{};
+  dramConfig.setCacheSize(totalCacheSize);
+  return dramConfig;
+}
+
+TEST_F(LruMemoryTiersTest, TestStressInserts) {
+  std::vector<std::tuple<size_t, size_t, size_t, std::string>> stress_params = 
+  {
+    //params: total cache size, number of items to insert, size of data item, description
+    std::make_tuple(50 * 1024 * 1024, 1000, 256, "Cache is undersaturated, just a few data items"),
+    std::make_tuple(50 * 1024 * 1024, 108298, 256, "Data fills the entire 50Mb of DRAM-only cache"), 
+    std::make_tuple(50 * 1024 * 1024, 180000, 256, "Too much data for the cache size")
+  };
+
+  for(auto params: stress_params) {
+    LruAllocatorConfig tieredCacheConfig = create2TierCacheConfig(std::get<0>(params));
+    LruAllocatorConfig dramCacheConfig = createDramCacheConfig(std::get<0>(params));
+
+    std::cout << std::get<3>(params) << ":\n";
+    generateAndInsert(tieredCacheConfig, std::make_pair(0, std::get<1>(params)), std::get<2>(params));
+    generateAndInsert(dramCacheConfig, std::make_pair(0, std::get<1>(params)), std::get<2>(params));
+    std::cout << "Done\n";
+  }
+}
 } // namespace tests
 } // namespace cachelib
 } // namespace facebook
