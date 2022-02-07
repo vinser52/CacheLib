d13568ec212374188dd7699de0ddf38cc4891f60
victoria-mcgrath <victoria.mcgrath@intel.com>
2022-02-07 12:59:39 -0800

    Disabled test suite allocator-test-AllocatorTypeTest (#41)
    Disabled test suite allocator-test-AllocatorTypeTest to skip sporadically failing tests.

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 # Newline separated list of tests to ignore
 BLACKLIST="allocator-test-AllocationClassTest
+allocator-test-AllocatorTypeTest
 allocator-test-NvmCacheTests
 common-test-TimeTests
 common-test-UtilTests
@@ -12,3 +13,4 @@ if [ "$1" == "long" ]; then
 else
     find -type f \( -not -name "*bench*" -and -not -name "navy*" \) -executable | grep -vF "$BLACKLIST" | xargs -n1 bash -c
 fi
+# ./allocator-test-AllocatorTypeTest --gtest_filter=-*ChainedItemSerialization*:*RebalancingWithEvictions*
