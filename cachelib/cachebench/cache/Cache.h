8478fda4f5dad08cb342407a883f392a7bfd46c0
Igor Chorazewicz <igor.chorazewicz@intel.com>
2022-07-12 12:43:15 +0000

    Add option to print memory stats in bytes only

--- cachelib/cachebench/cache/Cache.h
+++ cachelib/cachebench/cache/Cache.h
@@ -44,7 +44,7 @@
 #include "cachelib/cachebench/util/NandWrites.h"
 
 DECLARE_bool(report_api_latency);
-DECLARE_bool(report_memory_usage_stats);
+DECLARE_string(report_memory_usage_stats);
 
 namespace facebook {
 namespace cachelib {
