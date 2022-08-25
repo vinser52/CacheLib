6122ab94fd689caa68ef2d797f8ecce51ff00f86
Daniel Byrne <byrnedj12@gmail.com>
2022-08-25 08:19:59 -0400

    remove extra whitespace

--- cachelib/allocator/CacheAllocator-inl.h
+++ cachelib/allocator/CacheAllocator-inl.h
@@ -1660,7 +1660,6 @@ CacheAllocator<CacheTrait>::tryEvictToNextMemoryTier(
   return {};
 }
 
-
 template <typename CacheTrait>
 typename CacheAllocator<CacheTrait>::WriteHandle
 CacheAllocator<CacheTrait>::tryEvictToNextMemoryTier(Item& item) {
